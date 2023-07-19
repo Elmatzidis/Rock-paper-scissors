a702e0b5c2e46329ab3617f5975ee706d6397a75
Paul <pauloselmatzidis2006@gmail.com>
2023-07-19 19:10:06 +0300

    Made a function for Score and disable buttons when user/pc wins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btns = document.querySelectorAll(".btns");
 const cScore = document.querySelector(".C-score");
 const pScore = document.querySelector(".P-score");
+const endResult = document.querySelector(".result");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -9,32 +10,46 @@ function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "its a draw";
+    return "It's a draw";
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    pScore.innerText = playerScore += 1;
+    pScore.innerText = ++playerScore;
     return "You have won! Rock beats Scissors";
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    pScore.innerText = playerScore += 1;
+    pScore.innerText = ++playerScore;
     return "You have won! Scissors beats Paper";
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    pScore.innerText = playerScore += 1;
+    pScore.innerText = ++playerScore;
     return "You have won! Paper beats Rock";
   } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    cScore.innerText = computerScore += 1;
+    cScore.innerText = ++computerScore;
     return "You have lost! Paper beats Rock";
   } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    cScore.innerText = computerScore += 1;
+    cScore.innerText = ++computerScore;
     return "You have lost! Scissors beats Paper";
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    cScore.innerText = computerScore += 1;
+    cScore.innerText = ++computerScore;
     return "You have lost! Rock beats Scissors";
   }
 }
 
-function Score() {
-  if (playerScore === 5 || computerScore === 5) {
+function disableButtons() {
+  btns.forEach((btn) => {
+    btn.disabled = true;
+  });
+}
+
+function checkResult() {
+  if (playerScore >= 5) {
+    endResult.innerHTML =
+      "<span>You have won!<br> Refresh the page to play more</span>";
+    disableButtons();
+  } else if (computerScore >= 5) {
+    endResult.innerHTML =
+      "<span>You have lost <br>Refresh the page to play more</span>";
+    disableButtons();
   }
 }
 
@@ -43,8 +58,6 @@ btns.forEach((btn) =>
     const playerSelection = choice.target.innerText;
     const computerSelection = getComputerChoice();
     const result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    console.log(playerSelection, playerScore);
-    console.log(computerSelection, computerScore);
+    checkResult();
   })
 );
